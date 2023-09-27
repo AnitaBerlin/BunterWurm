3024619728d7390a3e66a18cf9367a93bb76bce6
Florian Fritz Wiede <florian.wiede@helmholtz-berlin.de>
2023-09-27 11:00:39 +0200

    exakt abwechselndes blinken

--- arduino/Fritz/Blink/Blink.ino
+++ arduino/Fritz/Blink/Blink.ino
@@ -34,10 +34,9 @@ void loop() {
   digitalWrite(4, HIGH);  // turn the LED on (HIGH is the voltage level)
   delay(1000);                      // wait for a second
   digitalWrite(4, LOW);   // turn the LED off by making the voltage LOW
-  delay(1000);                     // wait for a second
+  //delay(1000);                     // wait for a second
   digitalWrite(13, HIGH);  // turn the LED on (HIGH is the voltage level)
   delay(500);                      // wait for a second
   digitalWrite(13, LOW);   // turn the LED off by making the voltage LOW
-  delay(500);                     // wait for a second
-  
+  //delay(500);                     // wait for a second
 }
